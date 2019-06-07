f07fe0d8a5b7bd1756c3c7b2436d0aa270aea8d7
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-07 11:22:27 +0800

    Code along during lecture

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -36,5 +38,19 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        console.log(err)
+        return res.status(500).json(err.message)
+      }
+      res.send()
+    })
+  } else {
+    res.end()
+  }
+})
 
 module.exports = router;
